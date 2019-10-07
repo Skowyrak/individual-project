deb282677df4979ecc513b2fe9a450e262ad4c36
Kasia Skowyra <skowyra.kasia@gmail.com>
2019-10-07 20:51:14 +0200

    add hamburger class

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function toggleMenu(visible) {
     document.querySelector('.sidebar').classList.toggle('show', visible)
   }
-  document.querySelector('.menu-icon').addEventListener('click', function(e) {
+  document.querySelector('.hamburger').addEventListener('click', function(e) {
       e.preventDefault();
       toggleMenu()
     });
